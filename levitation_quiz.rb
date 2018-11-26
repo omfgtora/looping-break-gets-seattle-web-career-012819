e0891e7965415237faf82f08d5aff9d9308da3dc
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-26 22:35:18 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,9 +1,12 @@
 
 def levitation_quiz
-	puts "What is the spell that enacts levitation?"
-	answer = gets.chomp
-	break if answer == "Wingardium Leviosa"
+  loop do
+  	puts "What is the spell that enacts levitation?"
+  	answer = gets.chomp
+  	break if answer == "Wingardium Leviosa"
 	end
+	puts "You passed the quiz!"
 end
 
 
+levitation_quiz
